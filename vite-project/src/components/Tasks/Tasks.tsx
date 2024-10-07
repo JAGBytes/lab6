9b21a2b70a3cb51e4f2ce831a779a4eada399311
Camila Torres <andrea.torres-g@mail.escuelaing.edu.co>
2024-10-07 18:23:25 -0500

    Agregando tareas

--- vite-project/src/components/Tasks/Tasks.tsx
+++ vite-project/src/components/Tasks/Tasks.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Task.module.css'
 
 type Props = {}
 
 export default function Tasks({}: Props) {
+    const [taskName, setTaskName] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
+    const [taskDate, setTaskDate] = useState('');
+    const [taskDifficulty, setTaskDifficulty] = useState('');
+    const [taskPriority, setTaskPriority] = useState('');
+    const [taskTime, setTaskTime] = useState('');
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskName(e.target.value);
+    }
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskDescription(e.target.value);
+    }
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskDate(e.target.value);
+    }
+    const handleDifficultyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskDifficulty(e.target.id);
+    }
+    const handlePriorityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskPriority(e.target.value);
+    }
+    const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskTime(e.target.value);
+    }
+
+    function addTask(){
+        console.log(taskName);
+        console.log(taskDescription);
+        console.log(taskDate);
+        console.log(taskDifficulty);
+        console.log(taskPriority);
+        console.log(taskTime);
+    }
+
   return (
     <main className={styles['main-container']}>
     <div className={styles['task-form']}>
@@ -14,6 +49,8 @@ export default function Tasks({}: Props) {
                 id="taskTitle" 
                 placeholder="Enter a title (no more than 30 characters)" 
                 maxLength={30} 
+                value={taskName}
+                onChange={handleNameChange}
             />
         </div>
         <div className={styles['form-group']}>
@@ -22,23 +59,34 @@ export default function Tasks({}: Props) {
                 id="taskDescription" 
                 placeholder="Enter a description (no more than 50 characters)" 
                 maxLength={50} 
+                value={taskDescription}
+                onChange={handleDescriptionChange}
             ></textarea>
         </div>
         <div className={styles['form-group']} id={styles['radio-b']}>
             <label><b>Difficulty</b></label>
             <div className={styles['form-group']}>
                 <span className={styles['opcion-radio']}>
-                    <input type="radio" id="high" name="taskDifficulty" value="High" />
+                    <input type="radio" id="high" name="taskDifficulty"  
+                    value="high"
+                    checked={ taskDifficulty === 'high' }
+                    onChange={handleDifficultyChange}/>
                     <label htmlFor="high">High</label>
                 </span>
 
                 <span className={styles['opcion-radio']}>
-                    <input type="radio" id="middle" name="taskDifficulty" value="Middle" />
+                    <input type="radio" id="middle" name="taskDifficulty" 
+                   value="middle"
+                   checked={ taskDifficulty === 'middle' }
+                   onChange={handleDifficultyChange}/>
                     <label htmlFor="middle">Middle</label>
                 </span>
 
                 <span className={styles['opcion-radio']}>
-                    <input type="radio" id="low" name="taskDifficulty" value="Low" />
+                    <input type="radio" id="low" name="taskDifficulty" 
+                    value="low"
+                    checked={ taskDifficulty === 'low' }
+                    onChange={handleDifficultyChange} />
                     <label htmlFor="low">Low</label>
                 </span>
             </div>
@@ -46,7 +94,7 @@ export default function Tasks({}: Props) {
 
         <div className={styles['form-group']}>
             <label htmlFor="taskPriority"><b>Priority</b></label>
-            <select id="taskPriority" className={styles['taskPriority']}>
+            <select id="taskPriority" className={styles['taskPriority']} value={taskPriority} onChange={handlePriorityChange}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -62,6 +110,8 @@ export default function Tasks({}: Props) {
                 placeholder="Enter average time" 
                 step="0.1" 
                 min="0" 
+                value={taskTime}
+                onChange={handleTimeChange}
             />
         </div>
         <div className={styles['date-container']}>
@@ -72,9 +122,11 @@ export default function Tasks({}: Props) {
                     <line x1="8" y1="2" x2="8" y2="6"></line>
                     <line x1="3" y1="10" x2="21" y2="10"></line>
                 </svg>
-                <input type="date" id="taskDueDate" />
+                <input type="date" id="taskDueDate" 
+                value={taskDate}
+                onChange={handleDateChange}/>
             </div>
-            <button /*onClick={() => addTask()}*/>Add</button>
+            <button onClick={() => addTask()}>Add</button>
         </div>
         <button /*onClick={() => generateRandomTasks()}*/>Generate Random Tasks</button>
     </div>
